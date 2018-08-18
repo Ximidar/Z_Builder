4e6f44e82fc3f791707c32ac1bd1ffeaec14a11c
Ximidar <ximidar@gmail.com>
2018-08-18 15:30:05 -0700

    Made the path assembler

--- package_assembler/path_builder/path_builder.go
+++ package_assembler/path_builder/path_builder.go
@@ -2,12 +2,13 @@
 * @Author: Ximidar
 * @Date:   2018-08-11 14:10:01
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-08-11 14:15:32
+* @Last Modified time: 2018-08-18 15:24:57
 */
 package path_builder
 
 import (
 	"fmt"
+	"path"
 )
 
 type PathBuilder struct{
@@ -27,12 +28,16 @@ func NewPathBuilder() *PathBuilder{
 	This function needs to return a string like the one above. Mostly this just needs to 
 	add the name of the png to a string
 */
-func (pb *PathBuilder) AlterPath(path string) (string, error){
+func (pb *PathBuilder) AlterPath(filepath string, expose_time string, add_number string) (string){
 
 	// Get the basename
+	filename := path.Base(filepath)
 
 	// construct the string
+	Z_path := fmt.Sprintf(`<file = "C:\Program Files\Vanquish\$$RecentJob\ET ULTRA_(ET ULTRA)_[258.03 x 161.32]__SI 500 __[vd50_w250]__(12.03.12_16.44.52)\%s" expose_time = %s add = %s>`,
+		filename, expose_time, add_number)
 
 	// return the string
+	return Z_path
 
 }
--- package_assembler/path_builder/path_builder_test.go
+++ package_assembler/path_builder/path_builder_test.go
@@ -2,5 +2,22 @@
 * @Author: Ximidar
 * @Date:   2018-08-11 14:10:11
 * @Last Modified by:   Ximidar
-* @Last Modified time: 2018-08-11 14:10:11
+* @Last Modified time: 2018-08-18 15:29:27
 */
+
+package path_builder_test
+
+import (
+	"testing"
+	"github.com/ximidar/Z_Builder/package_assembler/path_builder"
+)
+
+func Test_AlterPath(t *testing.T) {
+	pb := path_builder.NewPathBuilder()
+
+	test_path := pb.AlterPath("/test/this/item.png", "5000", "1")
+
+	if test_path != `<file = "C:\Program Files\Vanquish\$$RecentJob\ET ULTRA_(ET ULTRA)_[258.03 x 161.32]__SI 500 __[vd50_w250]__(12.03.12_16.44.52)\item.png" expose_time = 5000 add = 1>`{
+		t.Fatal("AlterPath did not assemble the string correctly")
+	}
+}
